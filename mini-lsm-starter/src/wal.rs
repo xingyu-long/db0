285d4e1e807784870313314a633aa352e74c9ddd
Xingyu Long <xingyulong97@gmail.com>
2025-10-29 13:29:20 -0700

    week2_day6: task1 wal

--- mini-lsm-starter/src/wal.rs
+++ mini-lsm-starter/src/wal.rs
@@ -15,15 +15,16 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
-use anyhow::Result;
-use bytes::Bytes;
+use anyhow::{Context, Result};
+use bytes::{Buf, BufMut, Bytes};
 use crossbeam_skiplist::SkipMap;
 use parking_lot::Mutex;
-use std::fs::File;
-use std::io::BufWriter;
+use std::fs::{File, OpenOptions};
+use std::io::{BufWriter, Read, Write};
 use std::path::Path;
 use std::sync::Arc;
 
+use crate::block::SIZEOF_U16;
 use crate::key::KeySlice;
 
 pub struct Wal {
@@ -32,15 +33,57 @@ pub struct Wal {
 
 impl Wal {
     pub fn create(_path: impl AsRef<Path>) -> Result<Self> {
-        unimplemented!()
+        Ok(Self {
+            file: Arc::new(Mutex::new(BufWriter::new(
+                OpenOptions::new()
+                    .read(true)
+                    .create_new(true)
+                    .write(true)
+                    .open(_path)
+                    .context("failed to create wal file")?,
+            ))),
+        })
     }
 
     pub fn recover(_path: impl AsRef<Path>, _skiplist: &SkipMap<Bytes, Bytes>) -> Result<Self> {
-        unimplemented!()
+        let path = _path.as_ref();
+        let mut file = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .open(path)
+            .context("failed to open wal file")?;
+
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+        let mut rbuf = buf.iter().as_slice();
+        while rbuf.has_remaining() {
+            let key_len = rbuf.get_u16() as usize;
+            let key = Bytes::copy_from_slice(&rbuf[..key_len]);
+            rbuf.advance(key_len);
+            let value_len = rbuf.get_u16() as usize;
+            let value = Bytes::copy_from_slice(&rbuf[..value_len]);
+            rbuf.advance(value_len);
+
+            _skiplist.insert(key, value);
+        }
+        Ok(Self {
+            file: Arc::new(Mutex::new(BufWriter::new(file))),
+        })
     }
 
+    // | key_len | key | value_len | value |
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        let mut file = self.file.lock();
+        let mut buf: Vec<u8> = Vec::with_capacity(SIZEOF_U16 * 2 + _key.len() + _value.len());
+        buf.put_u16(_key.len() as u16);
+        buf.put(_key);
+        buf.put_u16(_value.len() as u16);
+        buf.put(_value);
+
+        file.write_all(&buf)?;
+        file.flush().unwrap();
+
+        Ok(())
     }
 
     /// Implement this in week 3, day 5; if you want to implement this earlier, use `&[u8]` as the key type.
@@ -49,6 +92,8 @@ impl Wal {
     }
 
     pub fn sync(&self) -> Result<()> {
-        unimplemented!()
+        let mut file = self.file.lock();
+        file.get_mut().sync_all()?;
+        Ok(())
     }
 }
